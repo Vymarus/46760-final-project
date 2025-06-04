32dac2d1462cd22d01694c856fd5eabdbd5d3084
Vymarus <54576730+Vymarus@users.noreply.github.com>
2025-06-04 13:31:13 +0200

    Timer functionality added

---
+++ src/board.h
@@ -0,0 +1,18 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+// _TIMERINTERRUPT_LOGLEVEL_ from 0 to 4
+// Don't define _TIMERINTERRUPT_LOGLEVEL_ > 0. Only for special ISR debugging only. Can hang the system.
+// Don't define TIMER_INTERRUPT_DEBUG > 2. Only for special ISR debugging only. Can hang the system.
+#define TIMER_INTERRUPT_DEBUG       0
+#define _TIMERINTERRUPT_LOGLEVEL    0
+
+// Select only one to be true for SAMD21. Must must be placed at the beginning before #include "TimerInterrupt_Generic.h"
+#define USING_TIMER_TC3         true      // Only TC3 can be used for SAMD51
+#define USING_TIMER_TC4         false     // Not to use with Servo library
+#define USING_TIMER_TC5         false
+#define USING_TIMER_TCC         false
+#define USING_TIMER_TCC1        false
+#define USING_TIMER_TCC2        false     // Don't use this, can crash on some boards
+
+#endif // __BOARD_H__
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
+#include <board.h>
+#include <TimerInterrupt_Generic.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DAC_pin PIN_PA03
+#define ADC_pin PIN_PB02
+#define tempProbe PIN_PB03
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// TC3, TC4, TC5 max permissible TIMER_INTERVAL_MS is 1398.101 ms, larger will overflow, therefore not permitted
+// Use TCC, TCC1, TCC2 for longer TIMER_INTERVAL_MS
+#define TIMER_INTERVAL_MS        1000
+
+#define TIMER_DURATION_MS        5000
+
+volatile uint32_t preMillisTimer = 0;
+
+SAMDTimer ITimer(TIMER_TC3);
+
+void TimerHandler()
+{
+    static bool toggle = false;
+    digitalWrite(LED_BUILTIN, toggle);
+    toggle = !toggle;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+
+    while (!Serial && millis() < 5000);
+
+    delay(100);
+
+    Serial.print(F("\nStarting TimerInterruptTest on "));
+	Serial.println(BOARD_NAME);
+	Serial.println(SAMD_TIMER_INTERRUPT_VERSION);
+	Serial.println(TIMER_INTERRUPT_GENERIC_VERSION);
+	Serial.print(F("CPU Frequency = "));
+	Serial.print(F_CPU / 1000000);
+	Serial.println(F(" MHz"));
+
+    // Interval in millisecs
+	if (ITimer.attachInterruptInterval_MS(TIMER_INTERVAL_MS, TimerHandler))
+	{
+		preMillisTimer = millis();
+		Serial.print(F("Starting ITimer OK, millis() = "));
+		Serial.println(preMillisTimer);
+	}
+	else
+		Serial.println(F("Can't set ITimer. Select another freq. or timer"));
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    // Do nothing
 }
+
